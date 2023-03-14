069965386747cfddd5fd91d1a6d7adc9101158e4
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-03-14 09:39:55 +0100

    fix: prevent garbage collection here by removing the params variable

--- middleware/v1/params.go
+++ middleware/v1/params.go
@@ -8,17 +8,15 @@ import (
 	"github.com/opensaucerer/gotemplate/typing"
 )
 
-// ParamsF is a middleware that takes a request handler.
-// It sets a variable paramas and pushes it into the request's context
+// ParamsH is a middleware that takes a request handler
+// and pushes the request path parameters it into the request's context
 // using the ParamsCtxKey.
 func ParamsH() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//set variable params
-			params := mux.Vars(r)
 
-			//set params to the request context
-			ctx := context.WithValue(r.Context(), typing.ParamsCtxKey{}, params)
+			//set path parameter to the request context
+			ctx := context.WithValue(r.Context(), typing.ParamsCtxKey{}, mux.Vars(r))
 
 			//call next handler
 			next.ServeHTTP(w, r.WithContext(ctx))
@@ -27,17 +25,15 @@ func ParamsH() func(http.Handler) http.Handler {
 	}
 }
 
-// ParamsF is a middleware that takes a controller function.
-// It sets a variable paramas and pushes it into the request's context
+// ParamsF is a middleware that takes a controller function
+// and pushes the request path parameters into the request's context
 // using the ParamsCtxKey.
 func ParamsF() func(func(http.ResponseWriter, *http.Request)) http.Handler {
 	return func(next func(http.ResponseWriter, *http.Request)) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//set variable params
-			params := mux.Vars(r)
 
-			//set params to the request context
-			ctx := context.WithValue(r.Context(), typing.ParamsCtxKey{}, params)
+			//set path parameter to the request context
+			ctx := context.WithValue(r.Context(), typing.ParamsCtxKey{}, mux.Vars(r))
 
 			//call next handler
 			next(w, r.WithContext(ctx))
