be98a483d1f0a6d4769c240ded7cc76e85c8c350
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-03-16 13:23:01 +0100

    chores: added database connection env variables

--- typing/env.go
+++ typing/env.go
@@ -3,4 +3,8 @@ package typing
 type Env struct {
 	// Port for the server to listen on
 	Port string `env:"PORT"`
+	//AppDBUri is the monogodb connection string for App database
+	MongoDBUri string `env:"MONGO_DB_URI"`
+	//AppDBName is the database name for App
+	MongoDBName string `env:"MONGO_DB_NAME"`
 }
