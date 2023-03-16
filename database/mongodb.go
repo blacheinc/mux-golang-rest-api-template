dcbc44c3c01ba1e72f7aad75d0aa1932d2ff356f
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-03-16 13:24:47 +0100

    feat: new feature to create a mongoDB connection instantiator

---
+++ database/mongodb.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func NewMongoDBClient(uri string, name string) (*mongo.Database, error) {
+
+	clientOption := options.Client().ApplyURI(uri)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	//connect to mongoDB
+	client, err := mongo.Connect(ctx, clientOption)
+	if err != nil {
+		return nil, err
+	}
+	//check the connection
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+	log.Println("connected to monogodb!")
+
+	//get a handle to database
+	MongoDB = client.Database(name)
+
+	return MongoDB, nil
+}
