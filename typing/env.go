46d401fcba391dbb7df9416f9a4c8841ed303d9a
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-03-19 19:12:34 +0100

    fix

--- typing/env.go
+++ typing/env.go
@@ -4,7 +4,7 @@ type Env struct {
 	// Port for the server to listen on
 	Port string `env:"PORT"`
 	//AppDBUri is the monogodb connection string for App database
-	MongoDBUri string `env:"MONGO_DB_URI"`
+	MongoDBURI string `env:"MONGO_DB_URI"`
 	//AppDBName is the database name for App
 	MongoDBName string `env:"MONGO_DB_NAME"`
 }
