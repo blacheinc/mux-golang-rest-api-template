fe7576c115ab7fc8d52b9b44763a27c90cdc9fb7
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-03-19 18:45:02 +0100

    chores: added postgresqluri struct field

--- typing/env.go
+++ typing/env.go
@@ -3,4 +3,6 @@ package typing
 type Env struct {
 	// Port for the server to listen on
 	Port string `env:"PORT"`
+	//PostgreSQLURI is the PostgreSQL database uri string for App
+	PostgreSQLURI string `env:"POSTGRE_SQL_URI"`
 }
