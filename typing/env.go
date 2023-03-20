712846dc6d8438c9d9509a0285c244de52c04894
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-03-20 10:49:57 +0100

    fix: avoid two references to the same MongoDB connection pool by returning error

--- typing/env.go
+++ typing/env.go
@@ -3,8 +3,8 @@ package typing
 type Env struct {
 	// Port for the server to listen on
 	Port string `env:"PORT"`
-	//AppDBUri is the monogodb connection string for App database
+	//MongoDBURI is the monogoDB connection string for the app
 	MongoDBURI string `env:"MONGO_DB_URI"`
-	//AppDBName is the database name for APP
+	//MongoDBName is the MongoDB database name for the app
 	MongoDBName string `env:"MONGO_DB_NAME"`
 }
